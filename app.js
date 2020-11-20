8d81232b4e679968638e8661d85c26b7f842be8e
eltoredo <tanguy.e@hotmail.fr>
2020-11-20 18:24:49 +0100

    Corrected event listeners

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 (() => { 
 	let app = document.getElementById('app');
     let arrayToDisplay = [];
-	var buttons = [];
 
     for(let i = 0; i < 5; i++) {
         arrayToDisplay.push([]);
         for(let j = 0; j < 5; j++){
             arrayToDisplay[i].push(Math.random() < 0.7);
-            if (arrayToDisplay[i]) {
-            	buttons[i] = document.createElement('button');
 
-            	buttons[i].onclick = function() {
+            let button = document.createElement('button');
+			button.classList.add("cell", "white");
+			app.appendChild(button);
+
+            if (arrayToDisplay[i][j]) {
+            	button.onclick = function() {
 					// call function() create normal;
 				}
-
-				buttons[i].classList.add("cell", "white");
-				app.appendChild(buttons[i]);
             }
             else {
-            	buttons[i] = document.createElement('button');
-
-            	buttons[i].onclick = function() {
-					if(!alert('Alert For your User!'))
+            	button.onclick = function() {
+					if(!alert('You lost you fucking piece of shit'))
 						window.location.reload();
 				}
             }
         }
-		app.appendChild(buttons[i]);
     }
 
 })();
