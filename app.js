0f84c77671f033877c3175e7d3db18827a0a877f
eltoredo <tanguy.e@hotmail.fr>
2020-11-20 18:11:43 +0100

    Added css link and beginning of the creation

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 (() => { 
+	let app = document.getElementById('app');
     let arrayToDisplay = [];
-    for(let i= 0; i<5; i++) {
+	var buttons = [];
+
+    for(let i = 0; i < 5; i++) {
         arrayToDisplay.push([]);
-        for(let j=0; j<5; j++){
+        for(let j = 0; j < 5; j++){
             arrayToDisplay[i].push(Math.random() < 0.7);
+            if (arrayToDisplay[i]) {
+            	buttons[i] = document.createElement('button');
+
+            	buttons[i].onclick = function() {
+					// call function() create normal;
+				}
+
+				buttons[i].classList.add("cell", "white");
+				app.appendChild(buttons[i]);
+            }
+            else {
+            	buttons[i] = document.createElement('button');
+
+            	buttons[i].onclick = function() {
+					if(!alert('Alert For your User!'))
+						window.location.reload();
+				}
+            }
         }
+		app.appendChild(buttons[i]);
     }
+
 })();
