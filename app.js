46d577f382183e75d8372d8aab28964b99a3ed7b
Djamai Hichem <djamai@intechinfo.fr>
2020-11-20 17:48:21 +0100

    Tableau avec des bombes

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-(() => {
-
+(() => { 
+    let arrayToDisplay = [];
+    for(let i= 0; i<5; i++) {
+        arrayToDisplay.push([]);
+        for(let j=0; j<5; j++){
+            arrayToDisplay[i].push(Math.random() < 0.7);
+        }
+    }
 })();
