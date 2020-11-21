d13cf859b5525cd5ad4dfb8ab86dbe60ffca35d8
Alexandre Spitz <alexandre.spitz@ouidou.fr>
2020-11-21 14:23:22 +0100

    Corrected CSS and algorithm
    Problem with 0 and 1 int comparable to booleans

--- app.js
+++ app.js
@@ -1,85 +1,91 @@
 class Demineur {
-    //Array à deux dimensions
-    plan = [[]];
+  //Array à deux dimensions
+  plan = [[]];
 
-    setPlan(arrTwoDimension) {
-        this.plan = arrTwoDimension;
+  constructor() {
+      this.setPlan = this.setPlan.bind(this)
+      this.play = this.play.bind(this)
+      this.getBombsArround = this.getBombsArround.bind(this)
+      this.runThroughNeighboors = this.runThroughNeighboors.bind(this)
+  }
+
+  setPlan(arrTwoDimension) {
+    this.plan = arrTwoDimension;
+  }
+
+  play(x, y) {
+    if (this.plan[x][y]) {
+      this.plan[x][y] = this.getBombsArround(x, y).toString();
+      return this.plan[x][y];
+    } else return -1;
+  }
+
+  getBombsArround(x, y) {
+    let count = 0;
+    this.runThroughNeighboors(x, y, (rx, ry) => {
+      console.log(x + " " + y, this.plan[rx][ry]);
+      if (!this.plan[rx][ry] && typeof this.plan[rx, ry] !== "number") count++;
+    });
+    return count;
+  }
+
+  runThroughNeighboors(x, y, cb) {
+    console.log(this.plan);
+
+    for (let currentX = x - 1; currentX <= x + 1; currentX++) {
+      if (currentX < 0 || currentX >= this.plan.length) continue;
+      for (let currentY = y - 1; currentY <= y + 1; currentY++) {
+        if (currentY < 0 || currentY >= this.plan[currentX].length) continue;
+        if (currentX == x && currentY == y) continue;
+        cb.call(this, currentX, currentY);
+      }
     }
-
-    play(x, y) {
-        if(this.plan[x][y]) {
-            this.plan[x][y] = this.getBombsArround(x, y)
-            return this.plan[x][y];
-        } else return -1;
-    }
-
-    getBombsArround(x, y) {
-        let count = 0;
-        this.runThroughNeighboors(x, y, 
-            (rx, ry) =>{ 
-                console.log(x + " " + y, this.plan[rx][ry]);
-                if(!this.plan[rx][ry]) count ++;
-            } )
-        return count;
-    }
-
-    runThroughNeighboors(x, y, cb) {
-        console.log(this.plan);
-
-        for(let currentX = x - 1; currentX <= x + 1; currentX ++) {
-            if(currentX < 0 || currentX >= this.plan.length) continue;
-            for(let currentY = y - 1; currentY <= y + 1; currentY ++) {
-                if(currentY < 0 || currentY >=  this.plan[currentX].length) continue;
-                if(currentX == x && currentY == y) continue;
-                cb(currentX, currentY)
-            }
-        }
-    }
+  }
 }
 
+(() => {
+  let app = document.getElementById("app");
 
-(() => { 
-	let app = document.getElementById('app');
+  function initGrille(size) {
     let arrayToDisplay = [];
 
     let demineur = new Demineur();
 
-    let baseSize = 10;
+    let baseSize = size;
 
-    for(let i = 0; i < baseSize; i++) {
-        arrayToDisplay.push([]);
+    for (let i = 0; i < baseSize; i++) {
+      arrayToDisplay.push([]);
 
-        let breakLine = document.createElement('br');
-		app.appendChild(breakLine);
+      let breakLine = document.createElement("div");
+      breakLine.classList.add("lineFlex")
+      app.appendChild(breakLine);
 
-        for(let j = 0; j < baseSize; j++){
-            arrayToDisplay[i].push(Math.random() < 0.7);
+      for (let j = 0; j < baseSize; j++) {
+        arrayToDisplay[i].push(Math.random() < 0.80);
 
-            let button = document.createElement('button');
-			button.classList.add("cell");
-			app.appendChild(button);
+        let button = document.createElement("div");
+        button.classList.add("cell");
 
-            	button.onclick = function(_) {
-                    let playResult = demineur.play(i, j)
-                    if(playResult >= 0) {
-                       button.textContent = playResult
-                    } else {
-                        if(!alert('You lost you fucking piece of shit'))
-						Window.location.reload();
-                    }
-					// call function() create normal;
-					button.classList.add("white");
-				}
-            
-            
-            
-        }
+        button.onclick = function (_) {
+          let playResult = demineur.play(i, j);
+          if (playResult >= 0) {
+            button.textContent = playResult;
+            button.classList.add("discovered");
+            button.classList.add("s"+playResult.toString())
+          } else {
+            alert("You lost you fucking piece of shit")
+          }
+          // call function() create normal;
+          button.classList.add("white");
+        };
+        breakLine.appendChild(button)
+      }
+
+      demineur.setPlan(arrayToDisplay);
     }
 
-    demineur.setPlan(arrayToDisplay);
-    
 
+  }
+  initGrille(12)
 
-    
-    
 })();
